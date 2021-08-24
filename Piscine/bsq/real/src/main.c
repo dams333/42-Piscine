1831c7f910e401e2ba01bb7aa8590b7c8c1dc36a
Damien HUBLEUR <dhubleur@e3r6p3.42.fr>
2021-08-24 15:37:09 +0200

    Save Commit

--- Piscine/bsq/real/src/main.c
+++ Piscine/bsq/real/src/main.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 14:30:54 by dhubleur          #+#    #+#             */
-/*   Updated: 2021/08/24 15:29:53 by dhubleur         ###   ########.fr       */
+/*   Updated: 2021/08/24 15:37:04 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,12 @@ void	parse_and_solve(t_file file)
 	if(result == 1)
 		write(2, "map error\n", 10);
 	if(result == 2)
-		print_map_with_symbols(map, el, )
+		print_map_with_symbols(*map, *el);
+	if(result == 3)
+	{
+		solve(map);
+		print_map_with_symbols(*map, *el)
+	}
 }
 
 int	main(int argc, char **argv)
