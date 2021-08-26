9e727d08accb5b4d7f1256d58dfe158a634d5127
Damien HUBLEUR <dhubleur@e3r12p12.42.fr>
2021-08-26 14:59:14 +0200

    Save Commit

--- Piscine/bsq/srcs/solve/main.c
+++ Piscine/bsq/srcs/solve/main.c
@@ -6,13 +6,20 @@
 /*   By: dhubleur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 14:30:54 by dhubleur          #+#    #+#             */
-/*   Updated: 2021/08/24 22:58:03 by dhubleur         ###   ########.fr       */
+/*   Updated: 2021/08/25 22:25:00 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-void	parse_and_solve(t_file file)
+void	print_map_error(int i, int max)
+{
+	write(2, "map error\n", 10);
+	if (i < (max - 1))
+		write(1, "\n", 1);
+}
+
+void	parse_and_solve(t_file file, int i, int max)
 {
 	int			result;
 	t_elements	el;
@@ -20,13 +27,19 @@ void	parse_and_solve(t_file file)
 
 	result = parse(&file, &el, &map);
 	if (result == 1)
-		write(2, "map error\n", 10);
+		print_map_error(i, max);
 	if (result == 2)
+	{
 		print_map_with_symbols(map, el);
+		if (i < (max - 1))
+			write(1, "\n", 1);
+	}
 	if (result == 0)
 	{
 		solve(&map);
 		print_map_with_symbols(map, el);
+		if (i < (max - 1))
+			write(1, "\n", 1);
 	}
 }
 
@@ -39,7 +52,7 @@ int	main(int argc, char **argv)
 	{
 		file = open_file("/");
 		if (file.end != 0)
-			parse_and_solve(file);
+			parse_and_solve(file, 1, 2);
 		else
 			write(2, "map error\n", 10);
 	}
@@ -50,9 +63,9 @@ int	main(int argc, char **argv)
 		{
 			file = open_file(argv[i]);
 			if (file.end != 0)
-				parse_and_solve(file);
+				parse_and_solve(file, i, argc);
 			else
-				write(2, "map error\n", 10);
+				print_map_error(i, argc);
 		}
 	}
 }
